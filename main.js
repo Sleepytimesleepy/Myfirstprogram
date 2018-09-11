6f03e111c4bac21abbfeb331747d0ce73adab061
Sleepytimesleepy <trandrew@mail.fresnostate.edu>
2018-09-11 16:43:40 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var Myfirstprogram = /** @class */ (function () {
+    function Myfirstprogram() {
+        var argArray = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            argArray[_i] = arguments[_i];
+        }
+        this.printArgs(argArray);
+    }
+    Myfirstprogram.prototype.printArgs = function (argArray) {
+        console.log(argArray);
+    };
+    Myfirstprogram.prototype.runFirst = function (callback) {
+        console.log('this method runs first...');
+    };
+    Myfirstprogram.prototype.runLast = function () {
+        console.log('this method runs last');
+    };
+    return Myfirstprogram;
+}());
+var newInstance = new Myfirstprogram("a", "b", "c");
---
+++ main.ts
@@ -0,0 +1,19 @@
+
+
+class Myfirstprogram {
+    constructor(...argArray: Array<any>) {
+        this.printArgs(argArray);
+    }
+        printArgs(argArray){
+
+            console.log(argArray);
+        }
+
+        runFirst(callback: Function) {
+            console.log('this method runs first...');
+        }
+            runLast(){
+                console.log('this method runs last');
+            }  
+}
+const newInstance = new Myfirstprogram("a", "b", "c")
